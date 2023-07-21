e85991c3610718e9f189fa0d9982d46c754bf253
Muhammad Waqar <mwaqar666@yahoo.com>
2023-07-22 00:33:33 +0500

    Fixes

--- app/Http/Requests/Api/Order/CreateGuestOrderRequest.php
+++ app/Http/Requests/Api/Order/CreateGuestOrderRequest.php
@@ -4,22 +4,26 @@ namespace App\Http\Requests\Api\Order;
 
 use Illuminate\Validation\Rules\RequiredIf;
 
-class CreateGuestOrderRequest extends CreateOrderRequest {
-	final public function rules(): array {
+class CreateGuestOrderRequest extends CreateOrderRequest
+{
+	final public function rules(): array
+	{
 		$rules = parent::rules();
 
 		$rules["receiver_details.create_account"] = ["required", "boolean"];
-		$rules["receiver_details.billing.email"][] = $this->shouldCreateAccount() ? "unique:customers" : "";
-		$rules["receiver_details.billing.password"] = [new RequiredIf($this->shouldCreateAccount()), "string", "min:6", "confirmed"];
+		$rules["receiver_details.billing.email"][] = "unique:customers";
+		$rules["receiver_details.billing.password"] = ["nullable", "string", "min:6", "confirmed"];
 
 		return $rules;
 	}
 
-	final public function shouldCreateAccount(): bool {
-		return $this->input("receiver_details.create_account");
+	final public function shouldNotCreateAccount(): bool
+	{
+		return !$this->shouldCreateAccount();
 	}
 
-	final public function shouldNotCreateAccount(): bool {
-		return !$this->shouldCreateAccount();
+	final public function shouldCreateAccount(): bool
+	{
+		return $this->input("receiver_details.create_account");
 	}
 }
